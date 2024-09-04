b9218b74000100fef96c3d4650443fa058723271
DianaP2 <L24550326@outlook.com>
2024-09-03 20:31:21 -0600

    Add files via upload

---
+++ EVA1_10_OPERACIONES2/src/eva1_10_operaciones2/EVA1_10_OPERACIONES2.java
@@ -0,0 +1,29 @@
+package eva1_10_operaciones2;
+
+public class EVA1_10_OPERACIONES2 {
+
+    /**
+     * Diana Sanchez
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Concatenacion cadenas
+        //Operador de concatenacion
+        //Mismo simbolo con dif uso;
+        //Sobrecarga de operaciones
+        String nombre = "Diana";
+        String apellido = "Sanchez";
+        String nomCom;
+        
+        //Concatenar:
+        nomCom = nombre + " " + apellido;
+        System.out.println(nomCom);
+        
+        //Imprimir los datos del usuario:
+        System.out.println("Nombre del usuario " + nombre);
+        System.out.println("Apellido del usuario " + apellido);
+        System.out.println("Nombre completo del usuario " + nomCom);
+    }
+    
+}
